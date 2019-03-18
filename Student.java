3cdda82b4cd24c72a61b8068e78a0d803618dd7c
NirashaThennakoon <nmalinthi@gmail.com>
2019-03-19 04:39:55 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,16 @@
+class Student{
+
+	public static void main(String[] args){
+
+		System.out.println("First main method");
+
+	}
+
+	public static void main(String[] args, int x){
+
+		System.out.println("Second main method");
+
+	}
+
+
+}
